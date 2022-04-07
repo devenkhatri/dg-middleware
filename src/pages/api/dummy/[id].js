2a6250a9700d1c06fe1e868d4649a9bb1e9acea2
devenkhatri <deven.khatri@gmail.com>
2022-04-07 15:20:57 +0530

    Update [id].js

--- src/pages/api/dummy/[id].js
+++ src/pages/api/dummy/[id].js
@@ -3,11 +3,7 @@ export default async function handler(req, res) {
   try {
     const { id } = req.query || {};
     const data = require(`./data/${id}.json`);
-    if (data && data.length > 0) {
-      res.status(200).json(data);
-    } else {
-      res.status(200).json([]);
-    }
+    res.status(200).json(data || []);
   } catch (err) {
     console.error(err);
     res.status(500).json(error(err));
